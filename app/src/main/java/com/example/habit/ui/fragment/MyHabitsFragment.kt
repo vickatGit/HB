43c7f5840e025a4849daa41032485219b5657ebf
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-07-06 21:49:50 +0530

    navigation in myHabits to addHabit

--- app/src/main/java/com/example/habit/ui/fragment/MyHabitsFragment.kt
+++ app/src/main/java/com/example/habit/ui/fragment/MyHabitsFragment.kt
@@ -5,16 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.habit.R
+import com.example.habit.databinding.FragmentMyHabitsBinding
 
 class MyHabitsFragment : Fragment() {
+
+    private var _binding: FragmentMyHabitsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_habits, container, false)
+        _binding=FragmentMyHabitsBinding.inflate(inflater,container,false)
+        binding.newHabitContainer.setOnClickListener {
+            findNavController().navigate(R.id.action_myHabitsFragment_to_addHabitFragment)
+        }
+        return binding.root
     }
 
 }

f7de275a00ee92e1ece855d1c319a5005cd3b959
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-09-09 21:30:25 +0530

    progress section ui hide show logic is improved

--- app/src/main/java/com/example/habit/ui/model/Epoxy/UserProgressSectionEpoxyModel.kt
+++ app/src/main/java/com/example/habit/ui/model/Epoxy/UserProgressSectionEpoxyModel.kt
@@ -1,7 +1,6 @@
 package com.example.habit.ui.model.Epoxy
 
 import android.graphics.Color
-import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.Typeface
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,85 +19,74 @@ import kotlinx.coroutines.withContext
 import java.time.LocalDate
 import java.time.ZoneOffset
 
-data class UserProgressSectionEpoxyModel (
-    private val getHabits: GetHabitThumbsUseCase,
-    private val progressSection: HomeElements.UserProgressSection
+data class UserProgressSectionEpoxyModel(
+    private val progressSection: HomeElements.UserProgressSection,
+    val totalHabits: Int,
+    val completedHabits: Int,
+    val habits: MutableList<ProgressSectionHabit>
 ) : ViewBindingKotlinModel<UserProgressSectionLayoutBinding>(R.layout.user_progress_section_layout) {
     override fun UserProgressSectionLayoutBinding.bind() {
+
+
         userProgressSection.layoutParams.apply {
             this as RecyclerView.LayoutParams
             setMargins(
-                DpPxUtils.dpToPX(progressSection.marginLeft,userProgressSection.context),
-                DpPxUtils.dpToPX(progressSection.marginTop,userProgressSection.context),
-                DpPxUtils.dpToPX(progressSection.marginRight,userProgressSection.context),
-                DpPxUtils.dpToPX(progressSection.marginBottom,userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.marginLeft, userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.marginTop, userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.marginRight, userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.marginBottom, userProgressSection.context),
             )
             userProgressSection.setPadding(
-                DpPxUtils.dpToPX(progressSection.paddingLeft,userProgressSection.context),
-                DpPxUtils.dpToPX(progressSection.paddingTop,userProgressSection.context),
-                DpPxUtils.dpToPX(progressSection.paddingRight,userProgressSection.context),
-                DpPxUtils.dpToPX(progressSection.paddingBottom,userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.paddingLeft, userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.paddingTop, userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.paddingRight, userProgressSection.context),
+                DpPxUtils.dpToPX(progressSection.paddingBottom, userProgressSection.context),
             )
         }
 
-        if(progressSection.isAnimationOn) waveView.setAnimationSpeed(progressSection.animationSpeed.toInt()) else waveView.stopAnimation()
+        if (progressSection.isAnimationOn) waveView.setAnimationSpeed(progressSection.animationSpeed.toInt()) else waveView.stopAnimation()
         waveView.setFrontWaveColor(Color.parseColor(progressSection.progressPrimaryColor))
         waveView.setBehindWaveColor(Color.parseColor(progressSection.progressSecondaryColor))
-        view.background.setColorFilter(Color.parseColor(progressSection.progressBackgroundColor),PorterDuff.Mode.SRC_ATOP)
+        view.background.setColorFilter(
+            Color.parseColor(progressSection.progressBackgroundColor),
+            PorterDuff.Mode.SRC_ATOP
+        )
         view.layoutParams.apply {
-            width=DpPxUtils.dpToPX(progressSection.progressSize,view.context)
-            height=DpPxUtils.dpToPX(progressSection.progressSize,view.context)
+            width = DpPxUtils.dpToPX(progressSection.progressSize, view.context)
+            height = DpPxUtils.dpToPX(progressSection.progressSize, view.context)
         }
 
         totalProgress.setTextColor(Color.parseColor(progressSection.progressTypographyProperties.textColor))
-        totalProgress.textSize=progressSection.progressTypographyProperties.headerTextSize
-        val userTextStyle=
-            if(progressSection.progressTypographyProperties.headerTextStyle.toLowerCase()=="bold") Typeface.BOLD
-            else if(progressSection.progressTypographyProperties.headerTextStyle.toLowerCase()=="normal") Typeface.NORMAL
-            else if(progressSection.progressTypographyProperties.headerTextStyle.toLowerCase()=="italic") Typeface.ITALIC
-            else if(progressSection.progressTypographyProperties.headerTextStyle.toLowerCase()=="bold_italic") Typeface.BOLD_ITALIC
+        totalProgress.textSize = progressSection.progressTypographyProperties.headerTextSize
+        val userTextStyle =
+            if (progressSection.progressTypographyProperties.headerTextStyle.toLowerCase() == "bold") Typeface.BOLD
+            else if (progressSection.progressTypographyProperties.headerTextStyle.toLowerCase() == "normal") Typeface.NORMAL
+            else if (progressSection.progressTypographyProperties.headerTextStyle.toLowerCase() == "italic") Typeface.ITALIC
+            else if (progressSection.progressTypographyProperties.headerTextStyle.toLowerCase() == "bold_italic") Typeface.BOLD_ITALIC
             else Typeface.NORMAL
-        totalProgress.typeface= Typeface.create(totalProgress.typeface,userTextStyle)
+        totalProgress.typeface = Typeface.create(totalProgress.typeface, userTextStyle)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            getHabits(this).collectLatest {
-                val habits = mutableListOf<ProgressSectionHabit>()
-                var totalHabits = 0
-                var completedHabits = 0
-                it.forEach {habit ->
-                    if(habit.entries!=null) {
-                        if(habit.entries!!.isEmpty()) {
-                            totalHabits++
-                            habits.add(ProgressSectionHabit(habit.title!!,false))
-                        }
-                        habit.entries?.forEach {
-                            if (it.key == LocalDate.now()) {
-                                totalHabits++
-                                if(it.value.completed) ++completedHabits
-                                habits.add(ProgressSectionHabit(habit.title!!,it.value.completed))
-                            }
-                        }
-                    }else{
-                        totalHabits++
-                    }
-                }
-                waveView.progress=((completedHabits.toFloat() / totalHabits.toFloat()) * 1000f).toInt()
-                withContext(Dispatchers.Main){
-                    habitsRecycler.layoutManager=LinearLayoutManager(habitsRecycler.context)
-                    val habitsAdapter = SectionHabitsAdapter(habits,progressSection.progressTypographyProperties)
-                    habitsRecycler.adapter=habitsAdapter
-                }
 
-            }
-        }
+        waveView.progress =
+            ((completedHabits.toFloat() / totalHabits.toFloat()) * 1000f).toInt()
+
+        habitsRecycler.layoutManager = LinearLayoutManager(habitsRecycler.context)
+        val habitsAdapter = SectionHabitsAdapter(
+            habits,
+            progressSection.progressTypographyProperties
+        )
+        habitsRecycler.adapter = habitsAdapter
+
+
     }
-
 }
+
 
 data class ProgressSectionHabit(
     val habitName: String,
     val isCompleted: Boolean
 )
+
 fun LocalDate.toMilliseconds(): Long {
     return this.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
 }

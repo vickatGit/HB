5886cb98b43f34cf7fa4b0708386df3b269e6b5c
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-09-11 17:45:42 +0530

    selected bottomnavigation handled according to fragment changes

--- app/src/main/java/com/example/habit/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/habit/ui/activity/HomeActivity.kt
@@ -3,13 +3,18 @@ package com.example.habit.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.get
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.habit.R
 import com.example.habit.databinding.ActivityHomeBinding
 import com.example.habit.ui.activity.AddMembersActivity.AddMembersActivity
 import com.example.habit.ui.activity.ProfileActivity.ProfileActivity
 import com.example.habit.ui.activity.UserSearchActivity.UserSearchActivity
+import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,33 +25,63 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding=ActivityHomeBinding.inflate(layoutInflater)
-        val navHost=supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
-        navController=navHost.navController
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navController = navHost.navController
+        val navListener=NavigationBarView.OnItemSelectedListener {
+            when (it.itemId) {
                 R.id.home -> {
-                    navController.popBackStack(R.id.homeFragment,true)
+                    navController.popBackStack(R.id.homeFragment, true)
                     navController.navigate(R.id.homeFragment)
                     true
                 }
+
                 R.id.my_habits -> {
+                    navController.popBackStack(R.id.myHabitsFragment, true)
                     navController.navigate(R.id.myHabitsFragment)
                     true
                 }
+
                 R.id.mentees -> {
-                    startActivity(Intent(this@HomeActivity,UserSearchActivity::class.java))
-//                    navController.navigate(R.id.menteesFragment)
-                    true
+                    false
                 }
+
                 R.id.community -> {
-                    startActivity(Intent(this@HomeActivity,ProfileActivity::class.java))
+                    navController.popBackStack(R.id.communityFragment, true)
+                    startActivity(Intent(this@HomeActivity, UserSearchActivity::class.java))
+//                    startActivity(Intent(this@HomeActivity,ProfileActivity::class.java))
 //                    navController.navigate(R.id.communityFragment)
-                    true
+                    false
                 }
-                else -> {false}
+
+                else -> {
+                    false
+                }
             }
         }
+        navController.addOnDestinationChangedListener(NavController.OnDestinationChangedListener { controller, destination, arguments ->
+            binding.bottomNavigationView.setOnItemSelectedListener(null)
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    binding.bottomNavigationView.menu[0].isChecked=true
+                }
+
+                R.id.myHabitsFragment -> {
+                    binding.bottomNavigationView.menu[1].isChecked=true
+                }
+
+                R.id.menteesFragment -> {
+                    binding.bottomNavigationView.menu[2].isChecked=true
+                }
+
+                R.id.communityFragment -> {
+                    binding.bottomNavigationView.menu[3].isChecked=true
+                }
+            }
+            binding.bottomNavigationView.setOnItemSelectedListener(navListener)
+        })
+        binding.bottomNavigationView.setOnItemSelectedListener(navListener)
 
         setContentView(binding.root)
     }
